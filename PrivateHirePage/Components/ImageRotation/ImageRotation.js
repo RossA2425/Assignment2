561a526294b30f0ffdf165b48305bba8fc88db81
RossA2425 <ross.anderson0302@gmail.com>
2023-04-24 13:45:46 +0100

    wip: Adjustment to ImageRotation.js

--- PrivateHirePage/Components/ImageRotation/ImageRotation.js
+++ PrivateHirePage/Components/ImageRotation/ImageRotation.js
@@ -6,6 +6,13 @@ function rotateImages() {
   images[index].classList.remove('active');
   index = (index + 1) % images.length;
   images[index].classList.add('active');
+  
+  if (index === 0) {
+    // Reset the index when it reaches the end of the array
+    images[images.length - 1].classList.remove('active');
+    index = 0;
+    images[index].classList.add('active');
+  }
 }
 
 setInterval(rotateImages, 3000);
