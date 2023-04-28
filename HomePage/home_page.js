fa77f9b778485564e90b1c617c5624b5b299134e
RossA2425 <98879596+RossA2425@users.noreply.github.com>
2023-04-28 11:57:20 +0100

    Ross (#21)
    Co-authored-by: Howard <hhoward@deloitte.co.uk>
    Co-authored-by: Therese Quinn <theresequinn@deloitte.co.uk>

--- HomePage/home_page.js
+++ HomePage/home_page.js
@@ -13,19 +13,19 @@ radioButtons.forEach(function (radioButton) {
   radioButton.addEventListener("change", function () {
     if (s1.checked) {
       infos.innerHTML =
-        "<h1>Creed |||</h1><p>Adonis has been thriving in both his carrer and family life, but when a childhood friend and former boxing prodigy resurfaces, the face-off is more than a fight.</p>";
+        "<h1>Fire Of Love</h1><p> A woman's life takes an unexpected turn when she comes into the limelight thanks to a photographer. However, he expects something in return for his efforts.</p>";
     } else if (s2.checked) {
       infos.innerHTML =
-        "<h1>Creed |</h1><p>Adonis has been thriving in both his carrer and family life, but when a childhood friend and former boxing prodigy resurfaces, the face-off is more than a fight.</p>";
+        "<h1>Aftersun </h1><p>Twenty years after their last holiday at a fading vacation resort, Sophie reflects on the rare time spent with her loving and idealistic father Calum. At 11-years-old, as the world of adolescence creeps into Sophie's view, Calum struggles under the weight of life outside of fatherhood. Sophie's recollections become a powerful and heartrending portrait of their relationship, as she tries to reconcile the father she knew with the man she didn't.</p>";
     } else if (s3.checked) {
       infos.innerHTML =
-        "<h1>Creed |</h1><p>Adonis has been thriving in both his carrer and family life, but when a childhood friend and former boxing prodigy resurfaces, the face-off is more than a fight.</p>";
+        "<h1>One Fine Morning </h1><p>A young single mother raising an 8-year-old daughter struggles to take care of her father, who's been diagnosed with a neurodegenerative disease. While trying to secure a decent nursing home, she runs into a married friend and they begin an affair.</p>";
     } else if (s4.checked) {
       infos.innerHTML =
-        "<h1>Creed |||</h1><p>Adonis has been thriving in both his carrer and family life, but when a childhood friend and former boxing prodigy resurfaces, the face-off is more than a fight.</p>";
+        "<h1>The Grand Budapest Hotel</h1><p>Gustave H, a concierge, is wrongly framed for murder at the Grand Budapest Hotel. In the process of proving his innocence, he befriends a lobby boy.</p>";
     } else if (s5.checked) {
       infos.innerHTML =
-        "<h1>Creed |||</h1><p>Adonis has been thriving in both his carrer and family life, but when a childhood friend and former boxing prodigy resurfaces, the face-off is more than a fight.</p>";
+        "<h1>Mirror</h1><p>Using a nonlinear structure interlaced with dreams and flashbacks, director Andrei Tarkovsky creates a stream-of-consciousness meditation on war, memory and time that draws heavily on events from his own life. Tarkovsky's film alter ego is Alexei (Ignat Daniltsev), a dying man in his 40s whose commonplace interactions with his wife (Margarita Terekhova) and children summon up a host of memories, ranging from his parents' divorce to his time on the battlefields of World War II.</p>";
     }
   });
 });
@@ -33,62 +33,58 @@ radioButtons.forEach(function (radioButton) {
 // Each movie object has properties for each date, which are arrays of showtimes.
 
 var subjectObject = {
-    "Creed 3": {
-      "4-23-2023": ["17:00", "19:00", "21:00"],
-      "4-24-2023": ["17:00", "19:00", "21:00"],
-      "4-25-2023": ["17:00", "19:00", "21:00"],
-      "4-26-2023": ["17:00", "19:00", "21:00"],
-      "4-27-2023": ["17:00", "19:00", "21:00"],
-      "4-28-2023": ["17:00", "19:00", "21:00"],
-      "4-29-2023": ["17:00", "19:00", "21:00"],
+    "Fire of Love": {
+      "Monday": ["17:30", "19:00", "20:00"],
+      "Tuesday": ["17:30", "19:00", "20:00"],
+      "Wednesday": ["17:00", "18:30", "20:00"],
+      "Thursday": ["17:00", "18:30", "20:00"],
+      "Friday": ["12:00", "15:00", "18:30", "22:30"],
+      "Saturday": ["12:00", "15:00", "18:30", "22:30"],
+      "Sunday": ["12:00", "15:00", "18:30", "21:30"],
 
 
     },
-    "Evil Dead Rise": {
-      "4-23-2023": ["19:00", "21:30"],
-      "4-24-2023":  ["19:00", "21:30"],
-      "4-25-2023": ["19:00", "21:30"],
-      "4-26-2023":  ["19:00", "21:30"],
-      "4-27-2023":  ["19:00", "21:30"],
-      "4-28-2023": ["19:00", "21:30"],
-      "4-29-2023": ["19:00", "21:30"],
+    "Aftersun": {
+      "Monday": ["19:00", "20:30"],
+      "Tuesday": ["17:30","21:00"],
+      "Wednesday": ["18:30", "21:00"],
+      "Thursday": ["17:00", "19:00", "21:00"],
+      "Friday": ["12:00", "15:00", "22:30"],
+      "Saturday": ["11:30", "22:30"],
+      "Sunday": ["11:30", "15:00", "18:30"],
     },
-    "Fast X": {
-      "4-23-2023": ["17:00","18:00", "19:00", "20:00"],
-      "4-24-2023": ["17:00","18:00", "19:00", "20:00"],
-      "4-25-2023":["17:00","18:00", "19:00", "20:00"],
-      "4-26-2023": ["17:00","18:00", "19:00", "20:00"],
-      "4-27-2023":["17:00","18:00", "19:00", "20:00"],
-      "4-28-2023": ["17:00","18:00", "19:00", "20:00"],
-      "4-29-2023":["17:00","18:00", "19:00", "20:00"],
+
+    "One Fine Morning": {
+      "Monday": ["17:00", "19:00", "20:30"],
+      "Tuesday": ["17:00", "18:30", "20:30"],
+      "Wednesday": ["18:30", "20:00"],
+      "Thursday": ["17:30","20:30"],
+      "Friday": ["12:00", "15:00", "22:30"],
+      "Saturday": ["11:30", "16:00", "22:30"],
+      "Sunday": ["11:30", "15:00", "22:30"],
     },
-    "John Wick: Chapter 4": {
-      "4-23-2023":["19:00", "21:30"],
-      "4-24-2023": ["19:00", "21:30"],
-      "4-25-2023": ["19:00", "21:30"],
-      "4-26-2023": ["19:00", "21:30"],
-      "4-27-2023":["19:00", "21:30"],
-      "4-28-2023": ["19:00", "21:30"],
-      "4-29-2023": ["19:00", "21:30"],
+
+    "The Grand Budapest Hotel": {
+      "Monday": ["17:30", "19:00", "21:00"],
+      "Tuesday": ["17:40", "18:50", "20:30"],
+      "Wednesday": ["18:30", "20:00"],
+      "Thursday": ["19:30","21:00"],
+      "Friday": ["12:00", "15:00", "17:55", "22:30"],
+      "Saturday": ["11:30", "16:00", "22:30", ],
+      "Sunday": ["11:30", "15:00", "22:45"],
     },
-    "The Super Mario Bros": {
-      "4-23-2023": ["13:00", "15:00", "17:00"],
-      "4-24-2023": ["13:00", "15:00", "17:00"],
-      "4-25-2023": ["13:00", "15:00", "17:00"],
-      "4-26-2023": ["13:00", "15:00", "17:00"],
-      "4-27-2023": ["13:00", "15:00", "17:00"],
-      "4-28-2023":["13:00", "15:00", "17:00"],
-      "4-29-2023": ["13:00", "15:00", "17:00"],
+    
+    "Mirror": {
+      "Monday": ["17:30","20:30"],
+      "Tuesday": ["17:00", "18:30", "20:00"],
+      "Wednesday": ["17:00", "18:30", "20:30"],
+      "Thursday": ["17:00", "19:00", "21:00"],
+      "Friday": ["12:00", "15:00", "22:30"],
+      "Saturday": ["11:30", "16:00", "22:30"],
+      "Sunday": ["11:30", "15:00", "19:30"],
     },
-    "Top Gun: Maverick": {
-      "4-23-2023": ["17:00", "19:00", "21:00"],
-      "4-24-2023": ["17:00", "19:00", "21:00"],
-      "4-25-2023": ["17:00", "19:00", "21:00"],
-      "4-26-2023": ["17:00", "19:00", "21:00"],
-      "4-27-2023": ["17:00", "19:00", "21:00"],
-      "4-28-2023": ["17:00", "19:00", "21:00"],
-      "4-29-2023": ["17:00", "19:00", "21:00"],
-    },
+
+
   };
   // When the window loads, populate the movie dropdown with the movies in the subjectObject
   // and set up event handlers for the movie and date dropdowns.
